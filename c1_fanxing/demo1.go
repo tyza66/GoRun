2d7f2c4a8d575989e1227bf82e9c974b485ff296
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-19 17:03:27 +0800

    update

--- c1_fanxing/demo1.go
+++ c1_fanxing/demo1.go
@@ -5,10 +5,16 @@ import "fmt"
 func main() {
 	//go1.18版本开始出现了泛型
 	strs := []string{"giao1","giao2"}
-	printArray(strs)
+	printArray2(strs)
 }
 //传统的方法 使用的是断言的转换
 func printArray(arr []interface{}) {
+	for _,a := range arr {
+		fmt.Println(a)
+	}
+}
+
+func printArray2[T string|int](arr []T) {
 	for _,a := range arr {
 		fmt.Println(a)
 	}
