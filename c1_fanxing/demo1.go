b4af10838a8c2d6b8e10db2ca940d34c926ed4c7
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-19 17:04:56 +0800

    update

--- c1_fanxing/demo1.go
+++ c1_fanxing/demo1.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	//go1.18版本开始出现了泛型
 	strs := []string{"giao1","giao2"}
+	nums := []int{1,2,3}
 	printArray2(strs)
+	printArray2(nums)
 }
 //传统的方法 使用的是断言的转换
 func printArray(arr []interface{}) {
