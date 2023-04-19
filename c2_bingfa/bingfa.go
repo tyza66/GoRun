28d6a1655e78ebd60a0daed3e9c37c596cb06059
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-19 18:46:03 +0800

    update

--- c2_bingfa/bingfa.go
+++ c2_bingfa/bingfa.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	//go程
+	go say("Hello")
+	say("giao")
+}
+
+func say(s string){
+	for i := 0;i<5;i++{
+		time.Sleep(100*time.Millisecond)
+		fmt.Println(s)
+	}
 }
