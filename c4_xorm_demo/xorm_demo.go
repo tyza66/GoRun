8b39528ad11ffc361acb29a23569c83aae7265ef
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-20 09:40:44 +0800

    update

--- c4_xorm_demo/xorm_demo.go
+++ c4_xorm_demo/xorm_demo.go
@@ -1,10 +1,44 @@
 package main
 
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+	"xorm.io/xorm"
+)
+
 /**
  * Author: tyza66
  * Date: 2023/04/20 8:19
  * Github: https://github.com/tyza66
  **/
 func main() {
+	var (
+		userName  string = "root"
+		password  string = "123456"
+		ipAddress string = "127.0.0.1"
+		port      int    = 3306
+		dbName    string = "go_test"
+		charset   string = "utf8mb4"
+	)
 
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s", userName, password, ipAddress, port, dbName, charset)
+	engine, err := xorm.NewEngine("mysql", dataSourceName)
+	if err != nil {
+		fmt.Println("数据库连接失败")
+	}
+	type User struct {
+		Id      int64
+		Name    string
+		Salt    string
+		Age     int
+		Passwd  string    `xorm:"varchar(200)"`
+		Created time.Time `xorm:"created"`
+		Updated time.Time `xorm:"updated"`
+	}
+
+	err = engine.Sync(new(User))
+	if err != nil {
+		fmt.Println("同步表结构失败")
+	}
 }
